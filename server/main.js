2272a164bf5b6bc76d033d1fd85886c38efb2e65
Helen Tran <helentran64@gmail.com>
2025-11-21 20:39:29 -0500

    init server

---
+++ server/main.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mysqlPool = require("./config/db");
+const cors = require('cors');
+
+dotenv.config({ path: "./.env" });
+app.use(express.json());
+app.use(cors());
+
+// routes
+
+app.get("/test", (req, res) => {
+  res.send("Hello World from Express.js!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+mysqlPool.query("SELECT 1").then(() => {
+  console.log("Database connection successful");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
