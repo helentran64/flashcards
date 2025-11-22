4894f34b047a78c7375bdd5d2c176f7a0ae3a667
Helen Tran <helentran64@gmail.com>
2025-11-22 18:19:01 -0500

    added homepage for logged in users #13

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="homepage">
+  <div class="homepageNotSignedIn" v-if="userStore.isLoggedOut">
     <h1 class="font-weight-bold text-h2 my-8">The Ultimate Studying Tool</h1>
     <p class="text-h5">Create, study, and manage your flashcards with ease</p>
     <p class="text-h5">Learn faster with spaced repetition</p>
     <v-btn color="primary" class="my-8" to="sign-up">Get Started</v-btn>
     <v-btn variant="outlined" color="primary" class="my-8 mx-2">Find FlashCards</v-btn>
   </div>
+  <div v-if="userStore.isLoggedIn">
+    <v-container>
+      <p class="font-weight-bold text-h2">Hello, {{ userStore.user?.firstName }}</p>
+      <ListofCards />
+    </v-container>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/userStore'
+import ListofCards from '@/components/ListOfFlashcards.vue'
+const userStore = useUserStore()
+</script>
 <style scoped>
-.homepage {
+.homepageNotSignedIn {
   text-align: center;
   margin-top: 100px;
 }
