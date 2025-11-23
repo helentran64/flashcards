9d356b14a62ead68f66cef3fe44ee44d1e02c9d7
Helen Tran <helentran64@gmail.com>
2025-11-22 21:07:20 -0500

    Allows users to edit their decks #19

--- server/routes/deckRouter.ts
+++ server/routes/deckRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { createDeck, getDeckByUsername, deleteDeckById } from "../controllers/deckController";
+import { createDeck, getDeckByUsername, deleteDeckById, updateDeckById } from "../controllers/deckController";
 
 const router = express.Router();
 router.post("/create", createDeck);
 router.get("/get_by_username/:username", getDeckByUsername);
 router.delete("/delete/:deck_id", deleteDeckById);
+router.put("/update/:deckId", updateDeckById);
 
 export = router;
